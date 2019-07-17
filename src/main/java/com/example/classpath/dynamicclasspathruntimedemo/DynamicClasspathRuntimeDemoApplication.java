4dd8b401a6fee0e63e87008ad49d20665df1aaf8
Peter Stritzinger <der.stritzinger@gmail.com>
2019-07-17 12:12:13 +0200

    initial load

---
+++ src/main/java/com/example/classpath/dynamicclasspathruntimedemo/DynamicClasspathRuntimeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.classpath.dynamicclasspathruntimedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicClasspathRuntimeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicClasspathRuntimeDemoApplication.class, args);
+	}
+
+}
